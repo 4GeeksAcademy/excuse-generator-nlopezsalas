ae32ad9766d081c5ca60c8de2f58be16854edadc
Natalia <nlopezsalas@gmail.com>
2024-01-18 19:40:40 +0000

    Create the excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //variables
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //generar un item aleatorio de cada uno de los arrays
+  function randomNumber(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+    console.log("index: " + index);
+    return index;
+  }
+
+  //concatenarlos => probar con un console.log
+  function excuseGenerator() {
+    let string =
+      who[randomNumber(who)] +
+      " " +
+      action[randomNumber(action)] +
+      " " +
+      what[randomNumber(what)] +
+      " " +
+      when[randomNumber(when)];
+    return string;
+  }
+
+  //HTML
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
